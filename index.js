b434f2879b38c37e605f0e6804635cda8de11dbf
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2021-12-16 17:40:36 +0530

    color fix

--- index.js
+++ index.js
@@ -49,3 +49,27 @@ function slideright(){
     slide.style.backgroundImage=img_slide[++i];
     }
 }
+
+
+// ather lider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+  if(slideIndex-2<0)
+  x[x.length-1].style.display = "block";
+  else
+  x[slideIndex-2].style.display = "block";
+}
