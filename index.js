3e9d90a60e2d7ad6a6096de432b9e13878d8c6f6
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2021-12-17 19:42:51 +0530

    data changed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+window.addEventListener('resize', reportWindowSize);
+var cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+document.querySelector("#cart").innerHTML=cart.length;
+
 document.getElementById("msearch").addEventListener("click",stog);
 function stog(el){
     var sr=document.querySelector(".hsc");
@@ -52,24 +56,87 @@ function slideright(){
 
 
 // ather lider
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+
+var prodData= [
+  {
+    img:"https://cdn.shopify.com/s/files/1/0390/2985/products/01_WBG_RETINOL_SERUM_20ml_dede3213-fc9c-41c2-889b-3840973e5ff8_400x.jpg",
+    title:"Tea Tree Dandruff Control Shampoo",
+    price:300
+  },
+  {
+    img:"https://cdn.shopify.com/s/files/1/0390/2985/products/Conditioner_Front_400x.jpg",
+    title:"Tea Tree Dandruff Control Shampoo",
+    price:350
+  },
+  {
+    img:"https://cdn.shopify.com/s/files/1/0390/2985/products/ScalpSerum_Front_400x.jpg",
+    title:"Tea Tree Dandruff Control Shampoo",
+    price:600
+  },
+  {
+    img:"https://cdn.shopify.com/s/files/1/0390/2985/products/WBG_Front_400x.jpg",
+    title:"Tea Tree Dandruff Control Shampoo",
+    price:750
+  },
+  {
+    img:"https://cdn.shopify.com/s/files/1/0390/2985/products/01_NS_LISTING-15ML_b12244f6-28e7-4dcb-a0b4-ed542f538271_400x.jpg",
+    title:"Tea Tree Dandruff Control Shampoo",
+    price:399
+  }
+];
+const bestSeller=document.querySelector("#best-seller");
+const whatsnew=document.querySelector("#whatsnew");
+showData(bestSeller,prodData);
+showData(whatsnew,prodData);
+function showData(el,data){
+  el.innerHTML="";
+  var size=Math.floor(screen.width/270);
+  data=data.slice(0,size);
+  data.map((pr)=>{
+  var div=document.createElement("div");
+  div.setAttribute("class","p-card");
+  var img=document.createElement("img");
+  img.setAttribute("src",pr.img);
+  var h2=document.createElement("h2");
+  h2.innerHTML=pr.title;
+  var hr=document.createElement("hr");
+  var p=document.createElement("p");
+  p.innerHTML="₹ "+pr.price;
+  var p2=document.createElement("p");
+  p2.innerHTML='<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>';
+  var button=document.createElement("button");
+  button.innerHTML="add to cart";
+  button.addEventListener("click", function () {
+    console.log(pr);
+    addToCart(pr);
+});
+  div.append(img,h2,hr,p,p2,button);
+  el.append(div);
+  });
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+function reportWindowSize(){
+  showData(bestSeller,prodData);
+showData(whatsnew,prodData);
+if(screen.width>767)
+slide.style.backgroundImage=img_slide_bg[i];
+else
+slide.style.backgroundImage=img_slide[i];
+}
+
+
+
+
+
+function addToCart(elem) {        
+  var cartObj={
+      img: elem.img,
+      title: elem.title,
+      price: elem.price
   }
-  x[slideIndex-1].style.display = "block";  
-  if(slideIndex-2<0)
-  x[x.length-1].style.display = "block";
-  else
-  x[slideIndex-2].style.display = "block";
+  console.log(cart);
+  cart.push(cartObj);
+  document.querySelector("#cart").innerHTML=cart.length;
+  localStorage.setItem("cartItems", JSON.stringify(cart));
 }
