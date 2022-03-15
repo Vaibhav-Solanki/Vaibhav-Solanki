0f165c10c95e877756c6a171dff0fc9b630eb16f
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-15 13:27:10 +0530

    icon fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./comp/navbar";
+import Navbar from "./comp/Navbar";
+import Footer from "./comp/Footer";
 function App() {
-  return <Navbar></Navbar>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
