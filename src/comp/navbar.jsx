819d5bf8c226a2bbeb23ff82e908e8ed07e6bdf7
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-15 13:10:28 +0530

    header

---
+++ src/comp/navbar.jsx
@@ -0,0 +1,104 @@
+function Navbar() {
+  return (
+    <header className="p-3 mb-3 border-bottom">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
+          >
+            <svg
+              className="bi me-2"
+              width="40"
+              height="32"
+              role="img"
+              aria-label="Bootstrap"
+            >
+              {/* <use xlink:href="#bootstrap"></use> */}
+            </svg>
+          </a>
+
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="#" className="nav-link px-2 link-secondary">
+                Overview
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-dark">
+                Inventory
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-dark">
+                Customers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-2 link-dark">
+                Products
+              </a>
+            </li>
+          </ul>
+
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Search..."
+              aria-label="Search"
+            />
+          </form>
+
+          <div className="dropdown text-end">
+            <a
+              href="#"
+              className="d-block link-dark text-decoration-none dropdown-toggle"
+              id="dropdownUser1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://github.com/mdo.png"
+                alt="mdo"
+                width="32"
+                height="32"
+                className="rounded-circle"
+              />
+            </a>
+            <ul
+              className="dropdown-menu text-small"
+              aria-labelledby="dropdownUser1"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  New project...
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Profile
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
