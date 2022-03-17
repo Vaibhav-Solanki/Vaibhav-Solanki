69d15274dc3cf7c6716dada641270612fddb441a
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-17 08:05:13 +0530

    fix head

--- src/comp/navbar.jsx
+++ src/comp/navbar.jsx
@@ -1,24 +1,26 @@
+import logo from "../logo.png";
 function Navbar() {
   return (
-    <header className="p-3 mb-3 border-bottom">
+    <header className="p-3 mb-3 border-bottom blr">
       <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between">
           <a
             href="/"
             className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
           >
-            <svg
+            <img src={logo} alt="Logo" height="32" />
+            {/* <svg
               className="bi me-2"
               width="40"
               height="32"
               role="img"
               aria-label="Bootstrap"
             >
-              {/* <use xlink:href="#bootstrap"></use> */}
-            </svg>
+              <use xlink:href="#bootstrap"></use>
+            </svg> */}
           </a>
 
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <ul className="nav col-12 col-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
               <a href="#" className="nav-link px-2 link-secondary">
                 Overview
@@ -40,61 +42,6 @@ function Navbar() {
               </a>
             </li>
           </ul>
-
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input
-              type="search"
-              className="form-control"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-
-          <div className="dropdown text-end">
-            <a
-              href="#"
-              className="d-block link-dark text-decoration-none dropdown-toggle"
-              id="dropdownUser1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="32"
-                height="32"
-                className="rounded-circle"
-              />
-            </a>
-            <ul
-              className="dropdown-menu text-small"
-              aria-labelledby="dropdownUser1"
-            >
-              <li>
-                <a className="dropdown-item" href="#">
-                  New project...
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Profile
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Sign out
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </header>
